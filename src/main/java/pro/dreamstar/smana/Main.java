dd34239714db4760f63cbb55db6301371d39059f
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-07-16 13:25:02 +0200

    Add twitter config

--- src/main/java/pro/dreamstar/smana/Main.java
+++ src/main/java/pro/dreamstar/smana/Main.java
@@ -1,8 +1,22 @@
 package pro.dreamstar.smana;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to SMANA!");
+
+        //Twitter Configuration
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("your consumer key")
+                .setOAuthConsumerSecret("your consumer secret")
+                .setOAuthAccessToken("your access token")
+                .setOAuthAccessTokenSecret("your access token secret");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
         }
 }
