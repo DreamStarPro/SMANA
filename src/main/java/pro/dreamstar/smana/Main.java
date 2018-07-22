d95b58ddf690a6603ae39687fb85cdb03e3d5a3a
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-07-22 19:53:41 +0200

    Fix API Authentication
    - Retrieves tweets based on query

--- src/main/java/pro/dreamstar/smana/Main.java
+++ src/main/java/pro/dreamstar/smana/Main.java
@@ -1,9 +1,21 @@
 package pro.dreamstar.smana;
 
+import twitter4j.TwitterException;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to SMANA!");
+
+        // Retrieve Tweets for a Query
+        try {
+            List<String> tsunamiTextList = TwitterApp.searchtweets("tsunami");
+            System.out.println(tsunamiTextList);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
 
     }
 
